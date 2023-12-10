30cd226418838855ffbcc558619a97a78296b51e
JADHAV ASHISH <77589461+AshishJadhav45@users.noreply.github.com>
2023-12-11 00:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 
 # Specify the absolute path to the 'pipe.pkl' file
-file_path = r'pipe.pkl'
+file_path = r'Crickinfo/pipe.pkl'
 
 # Check if the file exists
 if os.path.exists(file_path):
