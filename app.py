dbaa961a3d2865f49db0c218f2ed4ec3dd75ea20
JADHAV ASHISH <77589461+AshishJadhav45@users.noreply.github.com>
2023-12-13 21:16:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,38 @@
-import os
-import pickle
 import streamlit as st
+import pickle
 import pandas as pd
 
-# Specify the absolute path to the 'pipe.pkl' file
-import requests
-
-file_path = 'https://github.com/AshishJadhav45/Crick_info/raw/main/pipe.pkl'
-response = requests.get(file_path)
-
-if response.status_code == 200:
-    with open('pipe.pkl', 'wb') as file:
-        file.write(response.content)
-else:
-    print(f"Failed to download file. Status code: {response.status_code}")
-
-
-
-# Check if the file exists
-if os.path.exists(file_path):
-    # Load the pickled object
-    try:
-        with open(file_path, 'rb') as file:
-            pipe = pickle.load(file)
-    except Exception as e:
-        st.error(f"Error loading pickled object: {e}")
-        st.stop()
-else:
-    st.error(f"Error: File '{file_path}' not found.")
-    # You might want to handle this error appropriately, for example, by exiting the script or providing a default object.
-
-teams = ['Sunrisers Hyderabad', 'Mumbai Indians', 'Royal Challengers Bangalore', 'Kolkata Knight Riders',
-         'Kings XI Punjab', 'Chennai Super Kings', 'Rajasthan Royals', 'Delhi Capitals']
+teams = ['Sunrisers Hyderabad',
+ 'Mumbai Indians',
+ 'Royal Challengers Bangalore',
+ 'Kolkata Knight Riders',
+ 'Kings XI Punjab',
+ 'Chennai Super Kings',
+ 'Rajasthan Royals',
+ 'Delhi Capitals']
 
 cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
-          'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
-          'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
-          'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
-          'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
-          'Sharjah', 'Mohali',]
+       'Chandigarh', 'Jaipur', 'Chennai', 'Cape Town', 'Port Elizabeth',
+       'Durban', 'Centurion', 'East London', 'Johannesburg', 'Kimberley',
+       'Bloemfontein', 'Ahmedabad', 'Cuttack', 'Nagpur', 'Dharamsala',
+       'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
+       'Sharjah', 'Mohali', 'Bengaluru']
 
+pipe = pickle.load(open('pipe.pkl','rb'))
 st.title('IPL Win Predictor')
 
-col1, col2 = st.columns(2)
+col1, col2 = st.beta_columns(2)
 
 with col1:
-    batting_team = st.selectbox('Select the batting team', sorted(teams))
+    batting_team = st.selectbox('Select the batting team',sorted(teams))
 with col2:
-    bowling_team = st.selectbox('Select the bowling team', sorted(teams))
+    bowling_team = st.selectbox('Select the bowling team',sorted(teams))
 
-selected_city = st.selectbox('Select host city', sorted(cities))
+selected_city = st.selectbox('Select host city',sorted(cities))
 
 target = st.number_input('Target')
 
-col3, col4, col5 = st.columns(3)
+col3,col4,col5 = st.beta_columns(3)
 
 with col3:
     score = st.number_input('Score')
@@ -64,25 +43,15 @@ with col5:
 
 if st.button('Predict Probability'):
     runs_left = target - score
-    balls_left = 120 - (overs * 6)
+    balls_left = 120 - (overs*6)
     wickets = 10 - wickets
-    crr = score / overs
-    rrr = (runs_left * 6) / balls_left
+    crr = score/overs
+    rrr = (runs_left*6)/balls_left
 
-    input_df = pd.DataFrame({
-        'batting_team': [batting_team],
-        'bowling_team': [bowling_team],
-        'city': [selected_city],
-        'runs_left': [runs_left],
-        'balls_left': [balls_left],
-        'wickets': [wickets],
-        'total_runs_x': [target],
-        'crr': [crr],
-        'rrr': [rrr]
-    })
+    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
 
     result = pipe.predict_proba(input_df)
     loss = result[0][0]
     win = result[0][1]
-    st.header(f"{batting_team} - {round(win * 100)}%")
-    st.header(f"{bowling_team} - {round(loss * 100)}%")
+    st.header(batting_team + "- " + str(round(win*100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
